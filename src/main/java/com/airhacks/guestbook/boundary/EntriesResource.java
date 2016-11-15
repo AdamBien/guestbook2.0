441caabc85a6e0825fa088474087998df4e7b9ce
Adam Bien <abien@adam-bien.com>
2016-11-15 08:02:09 +0100

    Business logik exposure via JAX-RS, HTTP, XML and JSON

---
+++ src/main/java/com/airhacks/guestbook/boundary/EntriesResource.java
@@ -0,0 +1,51 @@
+
+package com.airhacks.guestbook.boundary;
+
+import com.airhacks.guestbook.entity.GuestEntry;
+import java.net.URI;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Stateless
+@Path("entries")
+public class EntriesResource {
+
+    @Inject
+    GuestService service;
+
+    @GET
+    public List<GuestEntry> all() {
+        return service.all();
+    }
+
+    @GET
+    @Path("{id}")
+    public GuestEntry find(@PathParam("id") long id) {
+        return this.service.find(id);
+    }
+
+    @POST
+    public Response save(GuestEntry entry, @Context UriInfo info) {
+        GuestEntry created = this.service.save(entry);
+        long id = created.getId();
+        URI uri = info.
+                getAbsolutePathBuilder().
+                path("/" + id).
+                build();
+        return Response.
+                created(uri).
+                build();
+    }
+}
