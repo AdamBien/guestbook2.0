40e2b80a2601be16d498d5092f8cd0f900c83bb4
Adam Bien <abien@adam-bien.com>
2016-11-15 06:18:55 +0100

    Behavior implemented in a POJO

---
+++ src/main/java/com/airhacks/guestbook/boundary/GuestService.java
@@ -0,0 +1,30 @@
+
+package com.airhacks.guestbook.boundary;
+
+import com.airhacks.guestbook.entity.GuestEntry;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@Stateless
+public class GuestService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public GuestEntry save(GuestEntry entry) {
+        return this.em.merge(entry);
+
+    }
+
+    public List<GuestEntry> all() {
+        return this.em.
+                createNamedQuery(GuestEntry.findAll, GuestEntry.class).
+                getResultList();
+    }
+}
--- src/main/java/com/airhacks/guestbook/entity/GuestEntry.java
+++ src/main/java/com/airhacks/guestbook/entity/GuestEntry.java
@@ -9,6 +9,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ *
+ * @author airhacks.com
+ */
 @Entity
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
